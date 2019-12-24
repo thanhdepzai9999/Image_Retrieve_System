557440c8c6027ebe94350c51dab5fd271c3d409e
dunganhdhbk <dunganhbbr8897@gmail.com>
2019-12-24 20:51:40 +0700

    up server

--- front_end/src/ImgDialog.js
+++ front_end/src/ImgDialog.js
@@ -10,7 +10,7 @@ import Slide from '@material-ui/core/Slide'
 import { css } from '@emotion/core';
 // First way to import
 import { HashLoader } from 'react-spinners';
-// import axios from 'axios';
+import axios from 'axios';
 import { Button } from 'antd';
 
 
@@ -61,60 +61,53 @@ class ImgDialog extends React.Component {
   handleClose = () => {
     this.setState({ open: false })
   }
-  onClick = () =>{
-      console.log(this.state)
-      console.log(this.props.img)
-      this.setState({
-          loading: true
-      })
-      var showsearch = []
-
-      showsearch.push(
-        <div>
-            Truy van anh thanh cong
-        </div>
-    )
+  onClick = () => {
+    // console.log(this.state)
+    // console.log(this.props.img)
     this.setState({
-        show: showsearch,
-        
+      loading: true
     })
-    //   var apiBaseUrl = "http://localhost:9000/users/";
-    //   var self = this;
-    //   var payload={
-    //     "url": this.props.img,
-        
-    //   }
-    //   axios.post(apiBaseUrl+'', payload)
-    //  .then(function (response) {
-    //    console.log(response);
-    //    if(response.status === 200){
-    //      console.log("Retrieve successfull");
-    //      showsearch.push(
-    //          <div>
-    //              Truy van anh thanh cong
-    //          </div>
-    //      )
-    //      this.setState({
-    //          show: showsearch,
-    //            loading:false
+    var showsearch = []
+    this.setState({
+      show: showsearch,
 
-    //      })
+    })
+    // console.log(this.state)
+    console.log(this.props.img)
+    var apiBaseUrl = "http://0.0.0.0:9000/queryimage";
+    var self = this;
+    var payload = {
+      "image": this.props.img,
 
-    //     //  var uploadScreen=[];
-    //     //  uploadScreen.push(<UploadPage appContext={self.props.appContext} role={self.state.loginRole}/>)
-    //     //  self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
-    //    }
-    //    else if(response.status === 204){
-         
-    //      alert("Loi 204")
-    //    }
-    //  })
-    //  .catch(function (error) {
-    //    console.log(error);
-    //  });
-      
-      
-      
+    }
+    axios.post(apiBaseUrl, payload, {'headers': {'Content-Type':'application/json'}})
+      .then(function (response) {
+        console.log(response);
+        if (response.status === 200) {
+          console.log("Retrieve successfull");
+          showsearch.push(
+            <div>
+              Truy van anh thanh cong
+             </div>
+          )
+          self.setState({
+            show: showsearch,
+            loading: false
+
+          })
+
+        }
+        else if (response.status === 204) {
+
+          alert("Loi 204")
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+
+
   }
 
   render() {
@@ -151,25 +144,25 @@ class ImgDialog extends React.Component {
             className={classes.img}
           />
         </div>
-        <br/>
-        <div className ="container" style = {{padding: 10}}>
-        <h1>Tim kiem ket qua tuong tu</h1>
-        {/* <button onClick = {this.onClick}> Tim kiem</button> */}
-        <Button type="primary" icon="search" onClick = {this.onClick}>
-          Search
+        <br />
+        <div className="container" style={{ padding: 10 }}>
+          <h1>Tim kiem ket qua tuong tu</h1>
+          {/* <button onClick = {this.onClick}> Tim kiem</button> */}
+          <Button type="primary" icon="search" onClick={this.onClick}>
+            Search
         </Button>
-            <div className='sweet-loading'>
-                <HashLoader
-                css={override}
-                sizeUnit={"px"}
-                size={120}
-                color={'#123abc'}
-                loading={this.state.loading}
-                />
-            </div>
-            <div>
-                {this.state.show}
-            </div>
+          <div className='sweet-loading'>
+            <HashLoader
+              css={override}
+              sizeUnit={"px"}
+              size={120}
+              color={'#123abc'}
+              loading={this.state.loading}
+            />
+          </div>
+          <div>
+            {this.state.show}
+          </div>
         </div>
       </Dialog>
     )
---
+++ server/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect
+from flask import request, Response
+from flask import render_template
+from flask_cors import CORS, cross_origin
+from flask import jsonify
+
+
+
+app = Flask(__name__,template_folder= ".")
+app.config["CORS_HEADER"] = 'Content-Type'
+CORS(app,support_credentials=True)
+
+@app.route('/queryimage', methods = ['POST'])
+@cross_origin(support_credentials=True)
+def example():
+    print("hihi")
+    try:
+        string = request.json
+        image = string['image']
+        return "hihi",200
+    except:
+        return 404
+
+@app.route('/')
+def index():
+    return "<div> hello <div>"
+
+if __name__ == '__main__':
+    # app.secret_key = 'super secret key'
+    app.run(host='0.0.0.0',port=9000,debug=True)
